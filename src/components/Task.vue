08be754180a91988fed0b0becf7f4326138d989b
mtennekoon <marvan.tennekoon@fivium.co.uk>
2021-10-12 15:16:40 +0100

    initial project

---
+++ src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']"> <!-- if true 'reminder' else nothing, but 'task' class should always be present -->
+    <h3>{{ task.text }}</h3>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object
+  },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
+
+<style scoped>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :key="task.id" v-for="task in tasks"> <!--for each task create a div and put the h3 tag inside the div-->
+    <Task @delete-task="$emit('delete-task', task.id)" @toggle-reminder="$emit('toggle-reminder', task.id)"  :task="task"/>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array
+  },
+  components: {
+    Task
+  },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
+
+<style scoped>
+
+</style>
